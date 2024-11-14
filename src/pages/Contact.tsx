abaf5a7507f5bd73872ccfd118137484f15b27a7
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-11-14 11:48:36 +0100

    update contact, add image

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -29,16 +29,43 @@ const StyledH2 = styled.h2`
   font-size: ${fontSizes.xxLarge};
 `;
 
+const StyledH3 = styled.h3`
+    color: ${colors.dark};
+  margin: 15px;
+  padding: 10px;
+  text-align: center;
+  font-size: ${fontSizes.xLarge};
+
+`;
 const StyledP = styled.p`
   color: ${colors.dark};
   font-size: ${fontSizes.basic};
   margin: 10px;
   line-height: 1.5;
+  text-align: left;
+  padding-left: 15px;
+  text-indent: -15px;
+`;
+
+
+const StyledBlock = styled.div`
+  color: ${colors.dark};
+  font-size: ${fontSizes.basic};
+  margin: 10px;
+  line-height: 1.5;
+`;
+const StyledList = styled.ul`
+  list-style-type: none;
 `;
 
 const StyledLink = styled.a`
   color: ${colors.accent};
   font-size: ${fontSizes.large};
+`;
+
+const StyledImg = styled.img`
+  width: 100%;
+  border-radius: 16px;
 `;
 
 export const Contact = () => {
@@ -47,13 +74,16 @@ export const Contact = () => {
       <PageWrapper>
         <TextWrapper>
           <StyledH2>Kontakt</StyledH2>
-          <StyledP>
-            Kontakta mig via e-post: 
+          <StyledBlock>
+            <StyledP>Kontakta mig via e-post: </StyledP>
             <StyledLink href="mailto:karlsen1973@outlook.com"> karlsen1973@outlook.com</StyledLink>
-          </StyledP>
-          <StyledP>
-            Se mer på
-            <ul>
+          </StyledBlock>
+          <StyledBlock>
+            <StyledImg src="./assets/anna2.jpg"></StyledImg>
+          </StyledBlock>
+          <StyledBlock>
+            <StyledH3>Se mer på</StyledH3>
+            <StyledList>
               <li>{" "}
               <StyledLink href="https://www.linkedin.com/in/annakarlsen/">LinkedIn</StyledLink>
 
@@ -62,8 +92,8 @@ export const Contact = () => {
               <StyledLink href="https://github.com/annkar73">GitHub</StyledLink>
 
               </li>
-            </ul>
-          </StyledP>
+            </StyledList>
+          </StyledBlock>
         </TextWrapper>
       </PageWrapper>
     </>
