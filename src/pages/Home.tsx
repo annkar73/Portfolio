05443429f6964efa472c77864d52d77bcdc8e542
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-11-14 12:24:23 +0100

    add image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 // src/pages/Home.tsx
 import styled from 'styled-components';
-import { colors } from '../helpers/variables';
+import { breakpoints, colors } from '../helpers/variables';
 
 // Styla din Hero-image
 const HeroSection = styled.section`
@@ -21,13 +21,28 @@ const Title = styled.h1`
   text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.7);
 `;
 
+const StyledImg = styled.img`
+  width: 100%;
+  border-radius: 16px;
+  max-width: 200px;
+  margin: 25px;
+  position: absolute;
+  left: 20%;
+
+  @media (min-width: ${breakpoints.tablet}) {
+    left: 0;
+  }
+`;
+
 const Home = () => {
   return (
     <div>
       <HeroSection>
+      <StyledImg src='./assets/anna2.jpg'></StyledImg>
+
         <Title>Anna Karlsen</Title>
       </HeroSection>
-     
+
     </div>
   );
 };
